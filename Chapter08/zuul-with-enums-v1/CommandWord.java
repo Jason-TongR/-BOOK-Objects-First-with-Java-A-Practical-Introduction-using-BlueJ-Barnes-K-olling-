af3c009e1a778114328071d2c0991d105a4e21f6
Jason <2862304720@qq.com>
2025-12-26 09:32:49 +0800

    Add files via upload

---
+++ Chapter08/zuul-with-enums-v1/CommandWord.java
@@ -0,0 +1,11 @@
+/**
+ * Representations for all the valid command words for the game.
+ * 
+ * @author  Michael Kölling and David J. Barnes
+ * @version 7.0
+ */
+public enum CommandWord
+{
+    // A value for each command word, plus one for unrecognized commands.
+    GO, QUIT, HELP, UNKNOWN
+}

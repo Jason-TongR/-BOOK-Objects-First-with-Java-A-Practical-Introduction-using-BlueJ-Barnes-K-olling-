661745eb4f22aa63c78f68c3e2aa718294c6c9dc
Jason <2862304720@qq.com>
2026-01-13 22:41:40 +0800

    Add files via upload

---
+++ Chapter20/taxi-company-later-stage/Actor.java
@@ -0,0 +1,13 @@
+/**
+ * An actor in the taxi-company simulation.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 7.2
+ */
+public interface Actor
+{
+    /**
+     * Implement the actor's behavior.
+     */
+    void act();
+}
